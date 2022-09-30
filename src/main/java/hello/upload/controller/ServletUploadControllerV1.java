a33acc950b4b4a90492ed52e6963a4ab0e63949a
portuna85 <portuna85@gmail.com>
2022-09-30 18:53:07 +0900

    File Upload

--- src/main/java/hello/upload/controller/ServletUploadControllerV1.java
+++ src/main/java/hello/upload/controller/ServletUploadControllerV1.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
@@ -27,7 +28,9 @@ public class ServletUploadControllerV1 {
 
         String itemName = request.getParameter("itemName");
         log.info("itemName = {}", itemName);
+
         Collection<Part> parts = request.getParts();
+
         log.info("parts = {}", parts);
         return "upload-form";
     }
